8f567f6fe99779ca91721393c917b8ab4c057794
fredpan <fredpan0821@gmail.com>
2018-01-21 00:23:32 -0500

    Test version of SQLiteHelper

---
+++ app/src/main/java/com/doooge/timemanager/SpecificTask.java
@@ -0,0 +1,45 @@
+package com.doooge.timemanager;
+
+import java.util.Calendar;
+
+/**
+ * Created by fredpan on 2018/1/20.
+ */
+
+public class SpecificTask extends Task {
+
+    private boolean isCompleted;
+    private String startTime;
+    private String endTime;
+
+
+    public SpecificTask(String taskName, Calendar startTime, Calendar endTime) {
+        super(taskName);
+        setEndTime(startTime);
+        setEndTime(endTime);
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Calendar endTime) {
+        this.endTime = CalendarHelper.convertCal2UTC(endTime);
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Calendar startTime) {
+        this.startTime = CalendarHelper.convertCal2UTC(startTime);
+    }
+}
