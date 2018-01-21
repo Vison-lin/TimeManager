23d05dc96493f2f0bd984984f28d7ce0fa7877d5
fredpan <fredpan0821@gmail.com>
2018-01-21 02:06:59 -0500

    init

--- app/src/main/java/com/doooge/timemanager/Type.java
+++ app/src/main/java/com/doooge/timemanager/Type.java
@@ -1,13 +1,63 @@
 package com.doooge.timemanager;
 
+import java.util.ArrayList;
+
 /**
  * Created by fredpan on 2018/1/20.
  */
 
 public class Type {
 
+    private int id;
     private String name;
     private String color;
+    private ArrayList<Task> tasks;
+
+    public Type(String name, String color) {
+        this.color = color;
+        this.name = name;
+        tasks = new ArrayList<>();
+    }
 
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public boolean deleteTask(Task task) {
+        boolean result = tasks.remove(task);
+        return result;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
