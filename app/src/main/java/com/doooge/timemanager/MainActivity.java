6b385b7410f345e25c34702c3248d19ea9697f0a
fredpan <fredpan0821@gmail.com>
2018-01-21 02:08:00 -0500

    init: add sample codes for DB

--- app/src/main/java/com/doooge/timemanager/MainActivity.java
+++ app/src/main/java/com/doooge/timemanager/MainActivity.java
@@ -1,13 +1,26 @@
 package com.doooge.timemanager;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Sample code for DB
+        LocalDatabaseHelper ldb = new LocalDatabaseHelper(this);
+        ldb.insertToSpecificTaskTable("SpecificTaskTable", 1, "2017", "2018", 21);
+        ldb.insertToTaskTable("TaskTable", 22);
+        ldb.insertToTypeTable("TypeTable", "ColorInGreen");
+        ldb.showAllData(this);
+
+
     }
 }
