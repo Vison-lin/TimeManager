7fcbba1a48569dd14f76e9319f3c77ea6413a08c
fredpan <fredpan0821@gmail.com>
2018-01-27 01:54:34 -0500

    Implemented notification method in order to update Adapter

--- app/src/main/java/com/doooge/timemanager/SpecificTaskOverviewAdapter.java
+++ app/src/main/java/com/doooge/timemanager/SpecificTaskOverviewAdapter.java
@@ -1,6 +1,5 @@
 package com.doooge.timemanager;
 
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,21 +8,18 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+
 /**
  * Created by diana on 2018-01-26.
  */
 
 public class SpecificTaskOverviewAdapter extends BaseAdapter {
 
-    private Context mContext;
     private ArrayList<SpecificTask> specificTasks;
-    private LayoutInflater mInflater;
-    private Context context;
 
-    public SpecificTaskOverviewAdapter(ArrayList<SpecificTask> specificTasks, Context context) {
+
+    public SpecificTaskOverviewAdapter(ArrayList<SpecificTask> specificTasks) {
         this.specificTasks = specificTasks;
-        this.context = context;
-
     }
 
     @Override
@@ -32,7 +28,7 @@ public class SpecificTaskOverviewAdapter extends BaseAdapter {
     }
 
     @Override
-    public Object getItem(int position) {
+    public SpecificTask getItem(int position) {
 
         return specificTasks.get(position);
     }
@@ -46,18 +42,18 @@ public class SpecificTaskOverviewAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View view, ViewGroup viewGroup) {
         //Get view for row item
-        View rowView = LayoutInflater.from(context).inflate(R.layout.activity_daily_task_list, viewGroup, false);
+        View rowView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.activity_daily_task_list, viewGroup, false);
         TextView taskName = rowView.findViewById(R.id.taskName);
         TextView taskHour = rowView.findViewById(R.id.taskHour);
-
-        getItem(position);
-        //   ArrayList<SpecificTask> specificTasks = (ArrayList<SpecificTask>) getItem(position);
-        taskName.setText("aaa");
-        taskHour.setText("111");
-
-
+        SpecificTask specificTask = getItem(position);
+        taskName.setText(specificTask.getTaskName());
+        taskHour.setText("111");//TODO TO BE IMPLEMENTED
         return rowView;
     }
 
+    public void updateSpecificTaskOverviewAdapter(ArrayList<SpecificTask> newSpecificTasks) {
+        specificTasks = new ArrayList<>(newSpecificTasks);
+        this.notifyDataSetChanged();
+    }
 }
 
