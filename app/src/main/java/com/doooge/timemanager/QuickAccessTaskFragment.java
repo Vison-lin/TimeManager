41284779898c220201795ceb42ab1f38354ff984
TianyouDai <ddai060@uottawa.ca>
2018-01-31 16:42:12 -0500

    test

--- app/src/main/java/com/doooge/timemanager/QuickAccessTaskFragment.java
+++ app/src/main/java/com/doooge/timemanager/QuickAccessTaskFragment.java
@@ -2,11 +2,12 @@ package com.doooge.timemanager;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by diana on 2018-01-27.
@@ -14,13 +15,23 @@ import android.widget.ListView;
 
 public class QuickAccessTaskFragment extends Fragment {
     private ListView mListView;
+    private QuickAccessTaskAdapter adapter;
+    //private Button mButton;
+
+    private ArrayList<SpecificTask> specificTasks;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.activity_quick_access_task, container, false);
-        mListView = (ListView) rootView.findViewById(R.id.taskList);
+
+        mListView = (ListView) rootView.findViewById(R.id.taskTypeBtn);
+
+        adapter = new QuickAccessTaskAdapter(specificTasks);
+        mListView.setAdapter(adapter);
+        //mButton = (Button) rootView.findViewById(R.id.taskTypeBtn);
         return rootView;
     }
 
 }
+
