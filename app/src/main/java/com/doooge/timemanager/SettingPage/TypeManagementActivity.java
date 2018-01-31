bea6cb3e6a485e41afc6a906aa0e9bf700274052
Vison-lin <vison994@gmail.com>
2018-01-31 18:37:30 -0500

    TimeBar View update 1.4  -vison

--- app/src/main/java/com/doooge/timemanager/SettingPage/TypeManagementActivity.java
+++ app/src/main/java/com/doooge/timemanager/SettingPage/TypeManagementActivity.java
@@ -1,10 +1,35 @@
 package com.doooge.timemanager.SettingPage;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.doooge.timemanager.R;
 
 /**
  * Created by fredpan on 2018/1/26.
  */
 
 public class TypeManagementActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.type_mainpage);
+
+        Button addtype = findViewById(R.id.addType);
+        addtype.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TypeManagementActivity.this,TypeCreator.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+    }
 }
