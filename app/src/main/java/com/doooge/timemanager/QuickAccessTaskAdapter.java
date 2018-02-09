f6caf702acde0a8cda422b4ca81facf1a76cbb76
TianyouDai <ddai060@uottawa.ca>
2018-02-08 21:06:05 -0500

    -updated "quickATA" -updated "QATF"

--- app/src/main/java/com/doooge/timemanager/QuickAccessTaskAdapter.java
+++ app/src/main/java/com/doooge/timemanager/QuickAccessTaskAdapter.java
@@ -1,6 +1,7 @@
 package com.doooge.timemanager;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,14 +38,29 @@ public class QuickAccessTaskAdapter extends BaseAdapter {
     public long getItemId(int position) {
         return position;
     }
-
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(int i, View view, final ViewGroup viewGroup) {
 
         View rowView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.activity_task_item, viewGroup, false);
 
         Button taskTypeBlock = rowView.findViewById(R.id.taskTypeBtn);
+        System.out.println("====" + (taskTypeBlock.equals(null)));
+
+        //TODO To be deleted: Facked Type
+        //================================
         taskTypeBlock.setText("Jogging");
+        //================================
+
+        taskTypeBlock.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //TODO Correct
+                Intent intent = new Intent(viewGroup.getContext(), TaskCreator.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                viewGroup.getContext().startActivity(intent);
+            }
+        });
+
         return rowView;
     }
+
 }
--- app/src/main/java/com/doooge/timemanager/QuickAccessTaskFragment.java
+++ app/src/main/java/com/doooge/timemanager/QuickAccessTaskFragment.java
@@ -1,10 +1,12 @@
 package com.doooge.timemanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -40,7 +42,23 @@ public class QuickAccessTaskFragment extends Fragment {
         adapter = new QuickAccessTaskAdapter(specificTasks);
         mListView = rootView.findViewById(R.id.taskList);
         mListView.setAdapter(adapter);
+        ImageView viewAddTasksBtn = rootView.findViewById(R.id.addingTaskBtn);
+        viewAddTasksBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getActivity(), TaskCreator.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                getActivity().startActivity(intent);
+            }
+        });
+
+
+
+
         return rootView;
+
+
     }
 
 
