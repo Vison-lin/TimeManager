dae50e95095805a314567feb3b0e568b0c621945
TianyouDai <ddai060@uottawa.ca>
2018-02-16 18:18:20 -0500

    Merge branch 'master' of https://github.com/fredpan/TimeManager
    # Conflicts:
    #	app/src/main/java/com/doooge/timemanager/SpecificTaskCreator.java

--- app/src/main/java/com/doooge/timemanager/SettingPage/HelpActivity.java
+++ app/src/main/java/com/doooge/timemanager/SettingPage/HelpActivity.java
@@ -1,10 +1,20 @@
 package com.doooge.timemanager.SettingPage;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.doooge.timemanager.R;
 
 /**
  * Created by fredpan on 2018/1/26.
  */
 
 public class HelpActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+    }
+
 }
