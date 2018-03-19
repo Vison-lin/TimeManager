8e68578743a900e0d362a5d99368df9b5cef9d4a
TianyouDai <ddai060@uottawa.ca>
2018-03-18 20:42:03 -0400

    Merge branch 'master' of https://github.com/fredpan/TimeManager
    # Conflicts:
    #	app/src/main/res/layout/activity_daily_task_list.xml
    -updated SpecificTaskOverviewAdatper

--- app/src/main/java/com/doooge/timemanager/SpecificTaskOverviewAdapter.java
+++ app/src/main/java/com/doooge/timemanager/SpecificTaskOverviewAdapter.java
@@ -57,18 +57,18 @@ public class SpecificTaskOverviewAdapter extends BaseAdapter implements NumberPi
         TextView taskHour = rowView.findViewById(R.id.taskHour);
         Button taskType = rowView.findViewById(R.id.typeBtn);
         final SpecificTask specificTask = getItem(position);
-        int color =Integer.parseInt(specificTask.getType().getColor());
-        if(color==viewGroup.getResources().getColor(R.color.violet)){
-            taskType.setBackground(viewGroup.getResources().getDrawable(R.drawable.btn_bkdg_purple));
-        }else if(color==-1){
+        int color = Integer.parseInt(specificTask.getType().getColor());
+        if (color == viewGroup.getResources().getColor(R.color.violet)) {
+            taskType.setBackground(viewGroup.getResources().getDrawable(R.drawable.btn_bkgd_purple));
+        } else if (color == -1) {
             taskType.setBackground(viewGroup.getResources().getDrawable(R.drawable.btn_bkgd_default));
-        }else if(color==viewGroup.getResources().getColor(R.color.green)){
+        } else if (color == viewGroup.getResources().getColor(R.color.green)) {
             taskType.setBackground(viewGroup.getResources().getDrawable(R.drawable.btn_bkgd_green));
-        }else if(color==viewGroup.getResources().getColor(R.color.blue)){
+        } else if (color == viewGroup.getResources().getColor(R.color.blue)) {
             taskType.setBackground(viewGroup.getResources().getDrawable(R.drawable.btn_bkgd_blue));
-        }else if(color==viewGroup.getResources().getColor(R.color.red)){
+        } else if (color == viewGroup.getResources().getColor(R.color.red)) {
             taskType.setBackground(viewGroup.getResources().getDrawable(R.drawable.btn_bkgd_red));
-        }else if(color==viewGroup.getResources().getColor(R.color.yellow)){
+        } else if (color == viewGroup.getResources().getColor(R.color.yellow)) {
             taskType.setBackground(viewGroup.getResources().getDrawable(R.drawable.btn_bkgd_yellow));
         }
 
